c65b8d93833e06decd4ce019f97632b66e43e13a
OffDutyTaoist <eshamilton1@gmail.com>
2025-06-25 17:28:56 -0500

    Create storybot.js

---
+++ storybot.js
@@ -0,0 +1,35 @@
+// Declare story variables
+let adjective;
+let noun;
+let verb;
+let place;
+let adjective2;
+let noun2;
+
+// Assign initial values for first story
+adjective = "grumpy";
+noun = "goblin";
+verb = "sneezing";
+place = "haunted library";
+adjective2 = "sparkly";
+noun2 = "cupcakes";
+
+// Create the first story
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+// Output first story
+console.log("First story: " + firstStory);
+
+// Reassign variables for second story
+adjective = "cheerful";
+noun = "dragon";
+verb = "snoring";
+place = "volcano cave";
+adjective2 = "icy";
+noun2 = "pizza rolls";
+
+// Create the second story
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+// Output second story
+console.log("Second story: " + secondStory);
