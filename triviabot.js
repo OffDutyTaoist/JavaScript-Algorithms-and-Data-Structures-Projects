675343a9640dbcbda7ec9622839d58556bf323b9
OffDutyTaoist <eshamilton1@gmail.com>
2025-06-25 17:24:42 -0500

    Create triviabot.js

---
+++ triviabot.js
@@ -0,0 +1,26 @@
+// Greeting message
+console.log("Hello! I'm your coding fun fact guide!");
+
+// Bot info variables
+var botName = "Cal";
+var botLocation = "The Codeverse";
+var favoriteLanguage = "JavaScript";
+
+// Logging bot info using concatenation
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+// Coding fact using let and concatenation
+let codingFact = favoriteLanguage + " was originally created in just 10 days!";
+console.log(codingFact);
+
+// Reassign and log a new fact
+codingFact = favoriteLanguage + " can be used for both frontend and backend development.";
+console.log(codingFact);
+
+// Reassign and log a third fact
+codingFact = favoriteLanguage + " treats functions as first-class citizens.";
+console.log(codingFact);
+
+// Farewell message
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
