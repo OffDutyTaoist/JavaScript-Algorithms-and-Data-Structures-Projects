0027566aaf1e3899e56276ecee98c197c63f088c
OffDutyTaoist <eshamilton1@gmail.com>
2025-07-28 14:50:03 -0500

    Update pyramid_generator.js

--- pyramid_generator.js
+++ pyramid_generator.js
@@ -22,3 +22,29 @@ for (const row of rows) {
 }
 
 console.log(result);
+
+//different method
+
+function pyramid(char, rows, vertexUp) {
+  let result = '\n';
+
+  if (!vertexUp) {
+    for (let i = 1; i <= rows; i++) {
+      let spaces = ' '.repeat(rows - i);
+      let pattern = char.repeat(2 * i - 1);
+      result += spaces + pattern + '\n';
+    }
+  } else {
+    for (let i = rows; i >= 1; i--) {
+      let spaces = ' '.repeat(rows - i);
+      let pattern = char.repeat(2 * i - 1);
+      result += spaces + pattern + '\n';
+    }
+  }
+
+  return result;
+}
+
+console.log(pyramid("o", 4, false));
+
+console.log(pyramid("p", 5, true));
