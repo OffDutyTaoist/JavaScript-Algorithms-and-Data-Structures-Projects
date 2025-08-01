e5e18911193a89341bf3dec19b8918dffa67b758
OffDutyTaoist <eshamilton1@gmail.com>
2025-08-01 15:38:33 -0500

    Create longestWordFinder.js

---
+++ longestWordFinder.js
@@ -0,0 +1,11 @@
+function findLongestWordLength(str) {
+    return str
+        .split(' ')
+        .reduce((maxLength, word) => Math.max(maxLength, word.length), 0);
+}
+
+console.log(findLongestWordLength("The quick brown fox jumped over the lazy dog"));
+console.log(findLongestWordLength("May the force be with you"));
+console.log(findLongestWordLength("Google do a barrel roll"));
+console.log(findLongestWordLength("What is the average airspeed velocity of an unladen swallow"));
+console.log(findLongestWordLength("What if we try a super-long word such as otorhinolaryngology"));
